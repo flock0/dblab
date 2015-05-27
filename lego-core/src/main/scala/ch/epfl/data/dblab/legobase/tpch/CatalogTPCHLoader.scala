7b889f2b9ded1a379e852b1879bde94446cbd30f
Florian Chlan <florian@chlan.net>
2015-05-27 11:46:13 +0200

    Adapted TPCHQueries to use Catalog

---
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/tpch/CatalogTPCHLoader.scala
@@ -0,0 +1,54 @@
+package ch.epfl.data
+package dblab.legobase
+package tpch
+
+import utils.Utilities._
+import sc.pardis.annotations.{ deep, metadeep, dontLift, dontInline, needs }
+import queryengine._
+import tpch._
+import schema._
+import storagemanager._
+import sc.pardis.shallow.OptimalString
+import sc.pardis.types._
+import scala.reflect._
+import scala.reflect.runtime.universe._
+import scala.reflect.runtime.currentMirror
+
+@needs[(K2DBScanner, Array[_], OptimalString, Loader, Table)]
+@deep
+trait CatalogTPCHLoader
+
+/**
+ * A module that defines loaders for TPCH relations.
+ */
+object CatalogTPCHLoader {
+
+  @dontLift
+  val tpchSchemaName: String = "TPCH"
+  @dontLift
+  val tpchSchema: Schema = TPCHSchema.getSchema(Config.datapath, getScalingFactor)
+  @dontLift
+  val catalog: Catalog = Catalog(Map(tpchSchemaName -> tpchSchema))
+  @dontInline
+  def getTable(tableName: String): Table = tpchSchema.tables.find(t => t.name == tableName).get
+  @dontLift
+  def getScalingFactor: Double = Config.datapath.slice(Config.datapath.lastIndexOfSlice("sf") + 2, Config.datapath.length - 1).toDouble //TODO Pass SF to Config
+
+  import Loader.loadTable
+
+  def loadRegion() = catalog.getTuples(tpchSchemaName, "REGION")
+
+  def loadPartsupp() = catalog.getTuples(tpchSchemaName, "PARTSUPP")
+
+  def loadPart() = catalog.getTuples(tpchSchemaName, "PART")
+
+  def loadNation() = catalog.getTuples(tpchSchemaName, "NATION")
+
+  def loadSupplier() = catalog.getTuples(tpchSchemaName, "SUPPLIER")
+
+  def loadLineitem() = catalog.getTuples(tpchSchemaName, "LINEITEM")
+
+  def loadOrders() = catalog.getTuples(tpchSchemaName, "ORDERS")
+
+  def loadCustomer() = catalog.getTuples(tpchSchemaName, "CUSTOMER")
+}
