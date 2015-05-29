1d7e12bdca64d73b36b918cfa7cb70a6268d5d46
Florian Chlan <florian@chlan.net>
2015-05-29 18:03:04 +0200

    Added possibility to run CatalogQueries from sbt

--- lego-core/src/main/scala/ch/epfl/data/dblab/legobase/LegoInterpreter.scala
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/LegoInterpreter.scala
@@ -3,12 +3,13 @@ package dblab.legobase
 
 import utils.Utilities._
 import java.io.PrintStream
+import tpch.Queries
+import tpch.CatalogQueries
 
 /**
  * The main object for interpreting queries.
  */
 object LegoInterpreter extends LegoRunner {
-  import tpch.Queries._
 
   /**
    * Interprets the given TPCH query with the given scaling factor.
@@ -17,28 +18,50 @@ object LegoInterpreter extends LegoRunner {
    * @param scalingFactor the scaling factor for TPCH queries (TODO should be generalize)
    */
   def executeQuery(query: String, scalingFactor: Double): Unit = query match {
-    case "Q1"     => Q1(Config.numRuns)
-    case "Q2"     => Q2(Config.numRuns)
-    case "Q3"     => Q3(Config.numRuns)
-    case "Q4"     => Q4(Config.numRuns)
-    case "Q5"     => Q5(Config.numRuns)
-    case "Q6"     => Q6(Config.numRuns)
-    case "Q7"     => Q7(Config.numRuns)
-    case "Q8"     => Q8(Config.numRuns)
-    case "Q9"     => Q9(Config.numRuns)
-    case "Q10"    => Q10(Config.numRuns)
-    case "Q11"    => Q11(Config.numRuns)
-    case "Q12"    => Q12(Config.numRuns)
-    case "Q13"    => Q13(Config.numRuns)
-    case "Q14"    => Q14(Config.numRuns)
-    case "Q15"    => Q15(Config.numRuns)
-    case "Q16"    => Q16(Config.numRuns)
-    case "Q17"    => Q17(Config.numRuns)
-    case "Q18"    => Q18(Config.numRuns)
-    case "Q19"    => Q19(Config.numRuns)
-    case "Q20"    => Q20(Config.numRuns)
-    case "Q21"    => Q21(Config.numRuns)
-    case "Q22"    => Q22(Config.numRuns)
+    case "Q1"     => Queries.Q1(Config.numRuns)
+    case "Q2"     => Queries.Q2(Config.numRuns)
+    case "Q3"     => Queries.Q3(Config.numRuns)
+    case "Q4"     => Queries.Q4(Config.numRuns)
+    case "Q5"     => Queries.Q5(Config.numRuns)
+    case "Q6"     => Queries.Q6(Config.numRuns)
+    case "Q7"     => Queries.Q7(Config.numRuns)
+    case "Q8"     => Queries.Q8(Config.numRuns)
+    case "Q9"     => Queries.Q9(Config.numRuns)
+    case "Q10"    => Queries.Q10(Config.numRuns)
+    case "Q11"    => Queries.Q11(Config.numRuns)
+    case "Q12"    => Queries.Q12(Config.numRuns)
+    case "Q13"    => Queries.Q13(Config.numRuns)
+    case "Q14"    => Queries.Q14(Config.numRuns)
+    case "Q15"    => Queries.Q15(Config.numRuns)
+    case "Q16"    => Queries.Q16(Config.numRuns)
+    case "Q17"    => Queries.Q17(Config.numRuns)
+    case "Q18"    => Queries.Q18(Config.numRuns)
+    case "Q19"    => Queries.Q19(Config.numRuns)
+    case "Q20"    => Queries.Q20(Config.numRuns)
+    case "Q21"    => Queries.Q21(Config.numRuns)
+    case "Q22"    => Queries.Q22(Config.numRuns)
+    case "Q1c"    => CatalogQueries.Q1(Config.numRuns)
+    case "Q2c"    => CatalogQueries.Q2(Config.numRuns)
+    case "Q3c"    => CatalogQueries.Q3(Config.numRuns)
+    case "Q4c"    => CatalogQueries.Q4(Config.numRuns)
+    case "Q5c"    => CatalogQueries.Q5(Config.numRuns)
+    case "Q6c"    => CatalogQueries.Q6(Config.numRuns)
+    case "Q7c"    => CatalogQueries.Q7(Config.numRuns)
+    case "Q8c"    => CatalogQueries.Q8(Config.numRuns)
+    case "Q9c"    => CatalogQueries.Q9(Config.numRuns)
+    case "Q10c"   => CatalogQueries.Q10(Config.numRuns)
+    case "Q11c"   => CatalogQueries.Q11(Config.numRuns)
+    case "Q12c"   => CatalogQueries.Q12(Config.numRuns)
+    case "Q13c"   => CatalogQueries.Q13(Config.numRuns)
+    case "Q14c"   => CatalogQueries.Q14(Config.numRuns)
+    case "Q15c"   => CatalogQueries.Q15(Config.numRuns)
+    case "Q16c"   => CatalogQueries.Q16(Config.numRuns)
+    case "Q17c"   => CatalogQueries.Q17(Config.numRuns)
+    case "Q18c"   => CatalogQueries.Q18(Config.numRuns)
+    case "Q19c"   => CatalogQueries.Q19(Config.numRuns)
+    case "Q20c"   => CatalogQueries.Q20(Config.numRuns)
+    case "Q21c"   => CatalogQueries.Q21(Config.numRuns)
+    case "Q22c"   => CatalogQueries.Q22(Config.numRuns)
     case dflt @ _ => throw new Exception("Query " + dflt + " not supported!")
   }
 
