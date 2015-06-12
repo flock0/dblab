3c606c0dc486d7c44b778e5a2d822a7bc4447bd8
Florian Chlan <florian@chlan.net>
2015-06-12 12:54:35 +0200

    SQLParser: Added EXTRACT test

--- lego-core/src/test/scala/ch/epfl/data/dblab/legobase/parser/SQLParserTest.scala
+++ lego-core/src/test/scala/ch/epfl/data/dblab/legobase/parser/SQLParserTest.scala
@@ -9,9 +9,9 @@ object TPCHQueries {
   val q4 = "SELECT o_orderpriority, COUNT(*) AS order_count FROM orders WHERE o_orderdate >= DATE '1993-08-01' AND o_orderdate < DATE '1993-11-01' AND EXISTS (SELECT * FROM lineitem WHERE l_orderkey = o_orderkey AND l_commitdate < l_receiptdate) GROUP BY o_orderpriority ORDER BY o_orderpriority"
   val q5 = "SELECT n_name, SUM(l_extendedprice * (1 - l_discount)) AS revenue FROM customer, orders, lineitem, supplier, nation, region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'ASIA' AND o_orderdate >= DATE '1996-01-01' AND o_orderdate < DATE '1997-01-01' GROUP BY n_name ORDER BY revenue DESC"
   val q6 = "SELECT SUM(l_extendedprice * l_discount) AS revenue FROM lineitem WHERE l_shipdate >= DATE '1996-01-01' AND l_shipdate < DATE '1997-01-01' AND l_discount BETWEEN 0.09 - 0.01 AND 0.09 + 0.01 AND l_quantity < 24"
-  val q7 = "SELECT supp_nation, cust_nation, l_year, SUM(volume) AS revenue FROM (SELECT n1.n_name AS supp_nation, n2.n_name AS cust_nation, extract(year FROM l_shipdate) AS l_year, l_extendedprice * (1 - l_discount) AS volume FROM supplier, lineitem, orders, customer, nation n1, nation n2 WHERE s_suppkey = l_suppkey AND o_orderkey = l_orderkey AND c_custkey = o_custkey AND s_nationkey = n1.n_nationkey AND c_nationkey = n2.n_nationkey AND ((n1.n_name = 'UNITED STATES' AND n2.n_name = 'INDONESIA') OR (n1.n_name = 'INDONESIA' AND n2.n_name = 'UNITED STATES')) AND l_shipdate BETWEEN DATE '1995-01-01' AND DATE '1996-12-31') AS shipping ORDER BY supp_nation, cust_nation, l_year ORDER BY supp_nation, cust_nation, l_year"
-  val q8 = "SELECT o_year, SUM(CASE WHEN nation = 'INDONESIA' THEN volume ELSE 0 END) / SUM(volume) AS mkt_share FROM (SELECT extract(year FROM o_orderdate) AS o_year, l_extendedprice * (1 - l_discount) AS volume, n2.n_name AS nation FROM part, supplier, lineitem, orders, customer, nation n1, nation n2, region WHERE p_partkey = l_partkey AND s_suppkey = l_suppkey AND l_orderkey = o_orderkey AND o_custkey = c_custkey AND c_nationkey = n1.n_nationkey AND n1.n_regionkey = r_regionkey AND r_name = 'ASIA' AND s_nationkey = n2.n_nationkey AND o_orderdate between DATE '1995-01-01' AND DATE '1996-12-31' AND p_type = 'MEDIUM ANODIZED NICKEL') AS all_nations GROUP BY o_year ORDER BY o_year"
-  val q9 = "SELECT nation, o_year, SUM(amount) AS sum_profit FROM (SELECT n_name AS nation, extract(year FROM o_orderdate) AS o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity AS amount FROM part, supplier, lineitem, partsupp, orders, nation WHERE s_suppkey = l_suppkey AND ps_suppkey = l_suppkey AND ps_partkey = l_partkey AND p_partkey = l_partkey AND o_orderkey = l_orderkey AND s_nationkey = n_nationkey AND p_name LIKE '%ghost%') AS profit GROUP BY nation, o_year ORDER BY nation, o_year DESC"
+  val q7 = "SELECT supp_nation, cust_nation, l_year, SUM(volume) AS revenue FROM (SELECT n1.n_name AS supp_nation, n2.n_name AS cust_nation, EXTRACT(YEAR FROM l_shipdate) AS l_year, l_extendedprice * (1 - l_discount) AS volume FROM supplier, lineitem, orders, customer, nation n1, nation n2 WHERE s_suppkey = l_suppkey AND o_orderkey = l_orderkey AND c_custkey = o_custkey AND s_nationkey = n1.n_nationkey AND c_nationkey = n2.n_nationkey AND ((n1.n_name = 'UNITED STATES' AND n2.n_name = 'INDONESIA') OR (n1.n_name = 'INDONESIA' AND n2.n_name = 'UNITED STATES')) AND l_shipdate BETWEEN DATE '1995-01-01' AND DATE '1996-12-31') AS shipping ORDER BY supp_nation, cust_nation, l_year ORDER BY supp_nation, cust_nation, l_year"
+  val q8 = "SELECT o_year, SUM(CASE WHEN nation = 'INDONESIA' THEN volume ELSE 0 END) / SUM(volume) AS mkt_share FROM (SELECT EXTRACT(YEAR FROM o_orderdate) AS o_year, l_extendedprice * (1 - l_discount) AS volume, n2.n_name AS nation FROM part, supplier, lineitem, orders, customer, nation n1, nation n2, region WHERE p_partkey = l_partkey AND s_suppkey = l_suppkey AND l_orderkey = o_orderkey AND o_custkey = c_custkey AND c_nationkey = n1.n_nationkey AND n1.n_regionkey = r_regionkey AND r_name = 'ASIA' AND s_nationkey = n2.n_nationkey AND o_orderdate between DATE '1995-01-01' AND DATE '1996-12-31' AND p_type = 'MEDIUM ANODIZED NICKEL') AS all_nations GROUP BY o_year ORDER BY o_year"
+  val q9 = "SELECT nation, o_year, SUM(amount) AS sum_profit FROM (SELECT n_name AS nation, EXTRACT(YEAR FROM o_orderdate) AS o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity AS amount FROM part, supplier, lineitem, partsupp, orders, nation WHERE s_suppkey = l_suppkey AND ps_suppkey = l_suppkey AND ps_partkey = l_partkey AND p_partkey = l_partkey AND o_orderkey = l_orderkey AND s_nationkey = n_nationkey AND p_name LIKE '%ghost%') AS profit GROUP BY nation, o_year ORDER BY nation, o_year DESC"
   val q10 = "SELECT TOP 20 c_custkey, c_name, SUM(l_extendedprice * (1 - l_discount)) AS revenue, c_acctbal, n_name, c_address, c_phone, c_comment FROM customer, orders, lineitem, nation WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate >= DATE '1994-11-01' AND o_orderdate < DATE '1995-02-01' AND l_returnflag = 'R' AND c_nationkey = n_nationkey GROUP BY c_custkey, c_name, c_acctbal, c_phone, n_name, c_address, c_comment ORDER BY revenue DESC"
   val q11 = "SELECT ps_partkey, SUM(ps_supplycost * ps_availqty) AS \"value\" FROM partsupp, supplier, nation WHERE ps_suppkey = s_suppkey AND s_nationkey = n_nationkey AND n_name = 'UNITED KINGDOM' GROUP BY ps_partkey HAVING SUM(ps_supplycost * ps_availqty) > (SELECT SUM(ps_supplycost * ps_availqty) * 0.0001 FROM partsupp, supplier, nation WHERE ps_suppkey = s_suppkey AND s_nationkey = n_nationkey AND n_name = 'UNITED KINGDOM') ORDER BY \"value\" DESC"
   val q12 = "SELECT l_shipmode, SUM(CASE WHEN o_orderpriority = '1-URGENT' OR o_orderpriority = '2-HIGH' THEN 1 ELSE 0 END) AS high_line_count, SUM(CASE WHEN o_orderpriority <> '1-URGENT' AND o_orderpriority <> '2-HIGH' THEN 1 ELSE 0 END) AS low_line_count FROM orders, lineitem WHERE o_orderkey = l_orderkey AND l_shipmode in ('MAIL', 'SHIP') AND l_commitdate < l_receiptdate AND l_shipdate < l_commitdate AND l_receiptdate >= date '1994-01-01' AND l_receiptdate < date '1995-01-01' GROUP BY l_shipmode ORDER BY l_shipmode"
@@ -29,195 +29,175 @@ object TPCHQueries {
 
 class SQLParserTest extends FlatSpec {
 
+  val parser = SQLParser
+
   "SQLParser" should "parse Q1 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q1)
     r should not be None
   }
 
   it should "parse Q2 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q2)
     r should not be None
   }
 
   it should "parse Q3 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q3)
     r should not be None
   }
 
   it should "parse Q4 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q4)
     r should not be None
   }
 
   it should "parse Q5 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q5)
     r should not be None
   }
 
   it should "parse Q6 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q6)
     r should not be None
   }
 
   it should "parse Q7 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q7)
     r should not be None
   }
 
   it should "parse Q8 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q8)
     r should not be None
   }
 
   it should "parse Q9 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q9)
     r should not be None
   }
 
   it should "parse Q10 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q10)
     r should not be None
   }
 
   it should "parse Q11 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q11)
     r should not be None
   }
 
   it should "parse Q12 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q12)
     r should not be None
   }
 
   it should "parse Q13 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q13)
     r should not be None
   }
 
   it should "parse Q14 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q14)
     r should not be None
   }
 
   it should "parse Q15 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q15)
     r should not be None
   }
 
   it should "parse Q16 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q16)
     r should not be None
   }
 
   it should "parse Q17 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q17)
     r should not be None
   }
 
   it should "parse Q18 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q18)
     r should not be None
   }
 
   it should "parse Q19 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q19)
     r should not be None
   }
 
   it should "parse Q20 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q20)
     r should not be None
   }
 
   it should "parse Q21 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q21)
     r should not be None
   }
 
   it should "parse Q22 correctly" in {
-    val parser = SQLParser
     val r = parser.parse(TPCHQueries.q22)
     r should not be None
   }
 
   it should "parse simple SELECT/FROM correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT * FROM table t")
     r should not be None
   }
 
   it should "parse single expression correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT column1 FROM table")
     r should not be None
   }
 
   it should "parse explicit columns correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT column1, ident.column2 FROM table")
     r should not be None
   }
 
   it should "parse aggregate functions correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT SUM(column1), MIN(ident.column2) FROM table")
     r should not be None
   }
 
   it should "parse aliased expression correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT SUM(column1) AS theSum, MIN(ident.column2) AS minimum FROM table")
     r should not be None
   }
 
   it should "parse arithmetic operations correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT SUM(column1) / 10, MIN(ident.column2) + 125.50 FROM table")
     r should not be None
   }
 
   it should "parse GROUP BY with HAVING correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT column1 FROM table GROUP BY column1, table.column2 HAVING SUM(table.column3) > 12345")
     r should not be None
   }
 
   it should "parse WHERE correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT column1 FROM table WHERE column2 > 2.51 AND 1 != 0 AND table.column3 BETWEEN 5 AND 10")
     r should not be None
   }
 
   it should "parse dates correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT * FROM lineitem WHERE DATE '1998-09-02'")
     r should not be None
   }
 
   it should "parse LIMIT correctly" in {
-    val parser = SQLParser
     val r = parser.parse("SELECT * FROM table LIMIT 20")
     r should not be None
+  }
+
+  it should "parse EXTRACT correctly" in {
+    val r1 = parser.parse("SELECT EXTRACT(YEAR FROM some_date) FROM table")
+    val r2 = parser.parse("SELECT EXTRACT(MONTH FROM some_date) FROM table")
+    val r3 = parser.parse("SELECT EXTRACT(DAY FROM some_date) FROM table")
+
+    r1 should not be None
+    r2 should not be None
+    r3 should not be None
   }
 }
