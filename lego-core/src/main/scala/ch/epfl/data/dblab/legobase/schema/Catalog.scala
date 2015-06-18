c5e171066cf142d41d8bf4292ad601479c80e400
Florian Chlan <florian@chlan.net>
2015-06-18 16:46:28 +0200

    Catalog: Manually added Catalog traits

---
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/schema/Catalog.scala
@@ -0,0 +1,72 @@
+package ch.epfl.data
+package dblab.legobase
+package schema
+
+import sc.pardis.types.Tpe
+import scala.collection.mutable.ArrayBuffer
+
+trait Catalog {
+  def findSchema(name: String): Schema
+  def getSchemaOrElseCreate(name: String): Schema
+  def createAttribute(name: String, dataType: Tpe, constraints: Seq[Constraint] = List()): Attribute
+}
+
+trait Schema {
+  def stats: Statistics
+  def tables: Seq[Table]
+  def addTable(name: String, attributes: Seq[Attribute], fileName: String, rowCount: Long)
+  def dropTable(tableName: String)
+  def findTable(name: String): Table
+  def findAttribute(name: String): Option[Attribute]
+  def toString: String
+}
+
+trait Table {
+  def name: String
+  def attributes: Seq[Attribute]
+  def constraints: Seq[Constraint]
+  def findAttribute(name: String): Option[Attribute]
+  def primaryKey: Option[PrimaryKey]
+  def dropPrimaryKey
+  def foreignKeys: Seq[ForeignKey]
+  def foreignKey(foreignKeyName: String): Option[ForeignKey]
+  def dropForeignKey(foreignKeyName: String)
+  def addConstraint(cstr: Constraint)
+  def notNulls: Seq[NotNull]
+  def uniques: Seq[Unique]
+  def autoIncrement: Option[AutoIncrement]
+  def fileName: String
+  def toString: String
+}
+
+trait Attribute {
+  def name: String
+  def dataType: Tpe
+  def hasConstraint(con: Constraint)
+  def toString: String
+}
+
+sealed trait Constraint {
+  def toString: String
+}
+case class PrimaryKey(attributes: Seq[Attribute]) extends Constraint {
+  override def toString = "PrimaryKey(" + attributes.map(a => a.name).mkString(",") + ")"
+}
+case class ForeignKey(foreignKeyName: String, ownTable: String, referencedTable: String, attributes: Seq[(String, String)]) extends Constraint {
+  override def toString = "ForeignKey(" + attributes.map(_._1).mkString(",") + ") references " + referencedTable + "(" + attributes.map(_._2).mkString(",") + ")"
+  def foreignTable(implicit s: Schema): Table = s.findTable(referencedTable)
+  def thisTable(implicit s: Schema): Table = s.findTable(ownTable)
+  def matchingAttributes(implicit s: Schema): Seq[(Attribute, Attribute)] = attributes.map { case (localAttr, foreignAttr) => thisTable.findAttribute(localAttr).get -> foreignTable.findAttribute(foreignAttr).get }
+}
+case class NotNull(attribute: Attribute) extends Constraint {
+  override def toString = s"NotNull($attribute)"
+}
+case class Unique(attribute: Attribute) extends Constraint {
+  override def toString = s"Unique($attribute)"
+}
+case class AutoIncrement(attribute: Attribute) extends Constraint {
+  override def toString = s"Unique($attribute)"
+}
+object Compressed extends Constraint {
+  override def toString = "COMPRESSED"
+}
