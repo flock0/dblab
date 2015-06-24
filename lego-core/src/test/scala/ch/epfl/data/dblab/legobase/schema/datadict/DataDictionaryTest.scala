fa9f76fcacef5fd8c8c486057ba16067e9fc0f6c
Florian Chlan <florian@chlan.net>
2015-06-24 17:27:43 +0200

    DataDictionary tests: Removed obsolete tests (They will be in CatalogTest)

--- lego-core/src/test/scala/ch/epfl/data/dblab/legobase/schema/datadict/DataDictionaryTest.scala
+++ lego-core/src/test/scala/ch/epfl/data/dblab/legobase/schema/datadict/DataDictionaryTest.scala
@@ -40,38 +40,4 @@ class DataDictionaryTest extends FlatSpec {
     assert(seqs(1).START_VALUE[Int] == 0)
     assert(seqs(1).INCREMENT_BY[Int] == 1)
   }
-
-  // val DATAPATH = System.getenv("LEGO_DATA_FOLDER")
-  // if (DATAPATH != null) {
-  //   it should "load PART table correctly" in {
-  //     val cat = Catalog(Map("TPCH" -> TPCHSchema.getSchema(s"$DATAPATH/sf0.1/", 0.1)))
-  //     Loader.loadTable(cat, "TPCH", "PART")
-  //     val records = cat.getTuples("TPCH", "PART")
-  //     assert(records.size == 20000)
-  //     assert(records(0).P_PARTKEY[Int] == 1)
-  //     assert(records(19999).P_RETAILPRICE[Double] == 920.00)
-  //   }
-
-  //   it should "load PART table automatically" in {
-  //     val cat = Catalog(Map("TPCH" -> TPCHSchema.getSchema(s"$DATAPATH/sf0.1/", 0.1)))
-  //     val records = cat.getTuples("TPCH", "PART")
-  //     assert(records.size == 20000)
-  //     assert(records(0).P_PARTKEY[Int] == 1)
-  //     assert(records(19999).P_RETAILPRICE[Double] == 920.00)
-  //   }
-
-  //   it should "load PART only once" in {
-  //     val cat = Catalog(Map("TPCH" -> TPCHSchema.getSchema(s"$DATAPATH/sf0.1/", 0.1)))
-  //     val records = cat.getTuples("TPCH", "PART")
-  //     assert(records.size == 20000)
-  //     assert(records(0).P_PARTKEY[Int] == 1)
-  //     assert(records(19999).P_RETAILPRICE[Double] == 920.00)
-  //     val sameRecords = cat.getTuples("TPCH", "PART")
-  //     assert(sameRecords.size == 20000)
-  //     assert(sameRecords(0).P_PARTKEY[Int] == 1)
-  //     assert(sameRecords(19999).P_RETAILPRICE[Double] == 920.00)
-  //   }
-  // } else {
-  //   fail("Tests could not run because the environment variable `LEGO_DATA_FOLDER` does not exist.")
-  // }
 }
