988d19df9c77e097bc6d7348cbeae21db2ed1408
Florian Chlan <florian@chlan.net>
2015-07-15 10:55:58 +0200

    TPC-DS: Create trait to hold benchmark-specific configuration (First step to extract TPC-H specific configuration from LegoIntrepreter)

---
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/benchmarks/BenchConfig.scala
@@ -0,0 +1,22 @@
+package ch.epfl.data
+package dblab.legobase
+package benchmarks
+
+import schema.Schema
+
+trait BenchConfig {
+    /** The path from the project root to the file containing the DDL statements*/
+    def ddlPath: String
+    /** The path from the project root to the file containing the constraints */
+    def constraintsPath: String
+    /** Whether to validate the query results */
+    def checkResult: Boolean
+    /** The path from the project root to the folder with results for validation */
+    def resultsPath: String = ""
+    /** Optimizes the operator tree (if any optimizations are applicable) */
+    def optimize(schema: Schema, operatorTree: Operator): Operator 
+    /** Parses and interprets the given DDL and constraints from disk */
+    def interpret: Schema
+    /** Optionally adds statistics to the schema */
+    def addStats(schema: Schema)
+}
