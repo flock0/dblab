d0be4e68a934a31e6e311f1e8fa55d393d3ae025
Florian Chlan <florian@chlan.net>
2015-07-15 11:03:29 +0200

    BenchConfig: Fixed indentation

--- lego-core/src/main/scala/ch/epfl/data/dblab/legobase/benchmarks/BenchConfig.scala
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/benchmarks/BenchConfig.scala
@@ -5,18 +5,18 @@ package benchmarks
 import schema.Schema
 
 trait BenchConfig {
-    /** The path from the project root to the file containing the DDL statements*/
-    def ddlPath: String
-    /** The path from the project root to the file containing the constraints */
-    def constraintsPath: String
-    /** Whether to validate the query results */
-    def checkResult: Boolean
-    /** The path from the project root to the folder with results for validation */
-    def resultsPath: String = ""
-    /** Optimizes the operator tree (if any optimizations are applicable) */
-    def optimize(schema: Schema, operatorTree: Operator): Operator 
-    /** Parses and interprets the given DDL and constraints from disk */
-    def interpret: Schema
-    /** Optionally adds statistics to the schema */
-    def addStats(schema: Schema)
+  /** The path from the project root to the file containing the DDL statements*/
+  def ddlPath: String
+  /** The path from the project root to the file containing the constraints */
+  def constraintsPath: String
+  /** Whether to validate the query results */
+  def checkResult: Boolean
+  /** The path from the project root to the folder with results for validation */
+  def resultsPath: String = ""
+  /** Optimizes the operator tree (if any optimizations are applicable) */
+  def optimize(schema: Schema, operatorTree: Operator): Operator 
+  /** Parses and interprets the given DDL and constraints from disk */
+  def interpret: Schema
+  /** Optionally adds statistics to the schema */
+  def addStats(schema: Schema)
 }
