9b5505f5b5a1100702f6ce5d4682d1b0e10d0395
Florian Chlan <florian@chlan.net>
2015-07-15 11:10:37 +0200

    TPC-DS: Added config for TPC-H

---
+++ lego-core/src/main/scala/ch/epfl/data/dblab/legobase/benchmarks/TPCHConfig.scala
@@ -0,0 +1,58 @@
+package ch.epfl.data
+package dblab.legobase
+package benchmarks
+
+import schema.Schema
+
+object TPCHConfig extends BenchConfig {
+    
+  val ddlPath: String = "tpch/dss.ddl"
+  val constraintsPath: String = "tpch/dss.ri"
+  val checkResult: Boolean = True
+  
+  val resultsPath: String = "results/"
+  
+  def optimize(schema: Schema, operatorTree: Operator): Operator =
+    if (q != "Q19" && q != "Q16" && q != "Q22") 
+      new NaiveOptimizer(schema).optimize(operatorTree) 
+    else
+      operatorTree // TODO -- FIX OPTIMIZER FOR Q19
+  
+  def interpret: Schema = {
+    val ddlDefStr = scala.io.Source.fromFile("tpch/dss.ddl").mkString
+    val schemaDDL = DDLParser.parse(ddlDefStr)
+    val constraintsDefStr = scala.io.Source.fromFile("tpch/dss.ri").mkString
+    val constraintsDDL = DDLParser.parse(constraintsDefStr)
+    val schemaWithConstraints = schemaDDL ++ constraintsDDL
+    DDLInterpreter.interpret(schemaWithConstraints)
+  }
+  
+  def addStats(schema: Schema) = {
+    // TODO: These stats will die soon
+    schema.stats += "DISTINCT_L_SHIPMODE" -> 7
+    schema.stats += "DISTINCT_L_RETURNFLAG" -> 3
+    schema.stats += "DISTINCT_L_LINESTATUS" -> 2
+    schema.stats += "DISTINCT_L_ORDERKEY" -> schema.stats.getCardinality("LINEITEM")
+    schema.stats += "DISTINCT_L_PARTKEY" -> schema.stats.getCardinality("PART")
+    schema.stats += "DISTINCT_L_SUPPKEY" -> schema.stats.getCardinality("SUPPLIER")
+    schema.stats += "DISTINCT_N_NAME" -> 25
+    schema.stats += "DISTINCT_O_SHIPPRIORITY" -> 1
+    schema.stats += "DISTINCT_O_ORDERDATE" -> 365 * 7 // 7-whole years
+    schema.stats += "DISTINCT_O_ORDERPRIORITY" -> 5
+    schema.stats += "DISTINCT_O_ORDERKEY" -> schema.stats.getCardinality("LINEITEM")
+    schema.stats += "DISTINCT_O_CUSTKEY" -> schema.stats.getCardinality("CUSTOMER")
+    schema.stats += "DISTINCT_P_PARTKEY" -> schema.stats.getCardinality("PART")
+    schema.stats += "DISTINCT_P_BRAND" -> 25
+    schema.stats += "DISTINCT_P_SIZE" -> 50
+    schema.stats += "DISTINCT_P_TYPE" -> 150
+    schema.stats += "DISTINCT_PS_PARTKEY" -> schema.stats.getCardinality("PART")
+    schema.stats += "DISTINCT_PS_SUPPKEY" -> schema.stats.getCardinality("SUPPLIER")
+    schema.stats += "DISTINCT_PS_AVAILQTY" -> 9999
+    schema.stats += "DISTINCT_S_NAME" -> schema.stats.getCardinality("SUPPLIER")
+    schema.stats += "DISTINCT_S_NATIONKEY" -> 25
+    schema.stats += "DISTINCT_C_CUSTKEY" -> schema.stats.getCardinality("CUSTOMER")
+    schema.stats += "DISTINCT_C_NAME" -> schema.stats.getCardinality("CUSTOMER")
+    schema.stats += "DISTINCT_C_NATIONKEY" -> 25
+    schema.stats += "NUM_YEARS_ALL_DATES" -> 7
+  }
+}
