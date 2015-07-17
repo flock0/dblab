ee4ce24656ba49f33b07eedd074f9f078007c4fa
Florian Chlan <florian@chlan.net>
2015-07-17 16:19:47 +0200

    TPC-DS: Rewrote Q1 to use explicit JOINs

--- tpcds/Q1.sql
+++ tpcds/Q1.sql
@@ -1,11 +1,9 @@
-SELECT  COUNT(*) 
+SELECT  COUNT(*) AS cnt
 FROM store_sales
-    ,household_demographics 
-    ,time_dim, store
-WHERE ss_sold_time_sk = time_dim.t_time_sk   
-    AND ss_hdemo_sk = household_demographics.hd_demo_sk 
-    AND ss_store_sk = s_store_sk
-    AND time_dim.t_hour = 8
+INNER JOIN household_demographics ON store_sales.ss_hdemo_sk = household_demographics.hd_demo_sk 
+INNER JOIN store ON store_sales.ss_store_sk = store.s_store_sk
+INNER JOIN time_dim ON store_sales.ss_sold_time_sk = time_dim.t_time_sk
+WHERE time_dim.t_hour = 8
     AND time_dim.t_minute >= 30
     AND household_demographics.hd_dep_count = 5
     AND store.s_store_name = 'ese'
