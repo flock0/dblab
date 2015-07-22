fa39187a6df69673ae53e9b58736e882efc2d520
Florian Chlan <florian@chlan.net>
2015-07-22 12:05:47 +0200

    TPC-DS: Fixed small typo in Q31

--- tpcds/Q31.sql
+++ tpcds/Q31.sql
@@ -9,7 +9,7 @@ SELECT  i_item_id
  WHERE i_current_price BETWEEN 36 AND 36 + 30
  AND d_date BETWEEN DATE '1998-04-06' AND DATE '1998-04-06' + 60
  AND i_manufact_id IN (746,802,854,823)
- AND inv_quantity_on_h AND BETWEEN 100 AND 500
+ AND inv_quantity_on_hand BETWEEN 100 AND 500
  GROUP BY i_item_id,i_item_desc,i_current_price
  ORDER BY i_item_id
  LIMIT 100;
