651a2d66b5f6be3214802fb134938b852c5eb38f
Florian Chlan <florian@chlan.net>
2015-07-23 16:00:10 +0200

    TPC-DS: Rewrote join conditions in Q61

--- tpcds/Q61.sql
+++ tpcds/Q61.sql
@@ -2,19 +2,17 @@
 SELECT  dt.d_year
  	,item.i_category_id
  	,item.i_category
- 	,SUM(ss_ext_sales_price)
- FROM 	date_dim dt
- 	,store_sales
- 	,item
- WHERE dt.d_date_sk = store_sales.ss_sold_date_sk
- 	AND store_sales.ss_item_sk = item.i_item_sk
- 	AND item.i_manager_id = 1  	
+ 	,SUM(ss_ext_sales_price) AS sum
+ FROM date_dim dt
+ INNER JOIN store_sales ON d_date_sk = ss_sold_date_sk
+ INNER JOIN item ON ss_item_sk = i_item_sk
+ WHERE item.i_manager_id = 1  	
  	AND dt.d_moy=12
  	AND dt.d_year=2000
  GROUP BY 	dt.d_year
  		,item.i_category_id
  		,item.i_category
- ORDER BY       SUM(ss_ext_sales_price) desc,dt.d_year
+ ORDER BY       SUM(ss_ext_sales_price) DESC,dt.d_year
  		,item.i_category_id
  		,item.i_category
-LIMIT 100 ;
+LIMIT 100;
